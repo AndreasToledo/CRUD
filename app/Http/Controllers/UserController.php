5a63d06f44dd4465e225245988ff390832d46f81
Andreas Toledo <eduardo.araujo.toledo1993@gmail.com>
2025-03-13 20:31:28 -0300

    Configuração dos Controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+ 
+ 
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+ 
+class UserController extends Controller
+{
+    private $products = [
+        ['id' => 1, 'name' => 'havaianas', 'preco' => 100],
+        ['id' => 2, 'name' => 'tenis ', 'preco' => 200]
+    ];
+ 
+    public function index() {
+        return response()->json($this->products);
+    }
+ 
+    public function store(Request $request) {
+        return response()->json(['message' => 'Produto criado com sucesso!']);
+    }
+ 
+    public function show($id) {
+        return response()->json(['id' => $id, 'havaianas' => 'chinelo', 'preco' => 150]);
+    }
+ 
+    public function update(Request $request, $id) {
+        return response()->json(['message' => 'Produto atualizado com sucesso!']);
+    }
+ 
+    public function destroy($id) {
+        return response()->json(['message' => 'Produto removido com sucesso!']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+//use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\OrderController;
+ 
+// Rotas para Produtos
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+ 
+// Rotas para Usuários
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+ 
+// Rotas para Pedidos
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders/{id}', [OrderController::class, 'show']);
+Route::put('/orders/{id}', [OrderController::class, 'update']);
+Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
